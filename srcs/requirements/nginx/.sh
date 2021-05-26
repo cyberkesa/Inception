836fa34715d74604498abfea23670be0fdccc83d
Voor Winfred <vwinfred@su-g1.msk.21-school.ru>
2021-05-26 16:56:59 +0300

    Work version all containers. But error: Gateway 502

--- srcs/requirements/nginx/.sh
+++ srcs/requirements/nginx/.sh
@@ -1,1 +1,10 @@
+
+# service mysql start
+
+# echo "CREATE USER 'vwinfred'@'localhost';" | mysql
+# echo "CREATE DATABASE wordpress;" | mysql
+# echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'vwinfred'@'localhost';" | mysql
+# echo "FLUSH PRIVILEGES;" | mysql
+
+# service php7.3-fpm start
 nginx -g 'daemon off;'
